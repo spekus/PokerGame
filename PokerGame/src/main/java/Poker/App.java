01c9afd4a895c99bd89bc608ecf70ac57873e727
spekus <spekulents@gmail.com>
2019-06-16 10:57:57 +0300

    initial commit

---
+++ PokerGame/src/main/java/Poker/App.java
@@ -0,0 +1,15 @@
+package Poker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+    }
+}
