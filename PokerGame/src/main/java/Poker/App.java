29a5a4a001c98a67a9ae27c0d7ef7fe5ddc9f118
spekus <spekulents@gmail.com>
2019-06-16 23:50:57 +0300

    added constants instead of magic strings

--- PokerGame/src/main/java/Poker/App.java
+++ PokerGame/src/main/java/Poker/App.java
@@ -15,13 +15,15 @@ import static Poker.cardEvaluation.evaluateHand;
 public class App {
     static int totalPoints = 100;
     final static int BET_SIZE = 1;
+    final static int FULL_HAND_SIZE =5;
+    final static int END_TURN =0;
+    final static int END_GAME =9;
 
     public static void main(String[] args) {
         System.out.println("Hello Player!");
         Cards cards = new Cards(2, 14);
         cards.drawCards(5);
         gameLogicLoop(cards);
-
     }
 
     private static void gameLogicLoop(Cards cards) {
@@ -30,27 +32,24 @@ public class App {
             System.out.println("your cards are:" + cards.getCardsInHand().toString());
             System.out.println("please pick which card do you want to replace, 0 means no card, 9 means ending game");
             Scanner in = new Scanner(System.in);
-            int n = in.nextInt();
-            if (n == 0) {
-                totalPoints -= BET_SIZE;
-                cards.drawCards(5- cards.getCardsInHand().size());
+            int userInput = in.nextInt();
+            if (userInput == END_TURN) {
+                cards.drawCards(FULL_HAND_SIZE- cards.getCardsInHand().size());
                 cards.getCardsInHand().sort(Comparator.comparing(card -> card.getRank()));
                 System.out.println("FINAL HAND: " + cards.getCardsInHand().toString());
-                int points = evaluateHand(cards.getCardsInHand());
-                totalPoints += points;
-                System.out.println(String.format("You won %s amount of points, cost of bet - %s, total points are %s", points, BET_SIZE, totalPoints));
+                int pointsReceived = evaluateHand(cards.getCardsInHand());
+                totalPoints = totalPoints + pointsReceived - BET_SIZE;
+                System.out.println(String.format("You won %s amount of points, cost of bet - %s, total points are %s", pointsReceived, BET_SIZE, totalPoints));
                 cards.shuffleDeck();
-                cards.drawCards(5);
+                cards.drawCards(FULL_HAND_SIZE);
                 continue;
             }
-            if(n ==9){
+            if(userInput ==END_GAME){
                 break;
             }
-            cards.discardCard(cards.getCardsInHand().get(n - 1));
-
+            cards.discardCard(cards.getCardsInHand().get(userInput - 1)); // -1 used because people start calculating from 1 and computer scientists from 0
         }
         System.out.println("Your final score is - " + totalPoints);
-
     }
 
 
