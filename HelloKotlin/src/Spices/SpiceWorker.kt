16d7248e253d50856460cd3c1ceda2daac373145
spekus <spekulents@gmail.com>
2019-06-24 16:08:47 +0300

    Removing ignored files

--- HelloKotlin/src/Spices/SpiceWorker.kt
+++
@@ -1,7 +0,0 @@
-package Spices
-
-fun main(){
-//    println(Spice.SpiceContainer(Spice.Curry()).toString())
-//    val listOfSpices = listOf(Spice("chilli", "hot"), Spice("peper", "spicy"), Spice().makeSalt())
-//    println(listOfSpices.filter { x -> x.heat < 6 }.toList().map {spice -> spice.name})
-}
